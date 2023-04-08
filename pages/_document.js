6533194f1fbc91f85be62539a63662d1e06bda2c
Raven The wolf <theoccultcorner@gmail.com>
2023-04-08 12:43:35 -0700

    FGFD

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,19 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          <meta name="description" content="Earl Brothers Masonry was brought to you with 20 years of experience by three men born and built in Santa Maria, California. We may not be the largest company at the moment but our eyes are open to the bigger and greater futures of our customers and our company. We have experience in all phases of masonry that include brick, block, and stone. Do you need retaining walls or perhaps a block wall? Maybe a stone fire pit or a stone barbecue to make your friends jealous? We'll throw in a free estimate for brick to show you we mean business! We guarantee the quality and integrity of our jobs well done. We'll have local competitors shaking in their boots work boots. We believe that our quality of work will lead to a successful masonry company honored to service the central coast for years to come!" />
+          <meta name="keywords" content="brick, 
+                                 block, 
+                                 stone, 
+                                 masonry, 
+                                 orcutt, 
+                                 santa maria, 
+                                 central coast, 
+                                 retaining walls, 
+                                 stone fire pit, 
+                                 stone barbecue" />
+          <meta name="author" content="Tom Earl, Chris Earl" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="theme-color" content="#000000" />
           <link rel="shortcut icon" href="/img/favicon.png" />
           <link
