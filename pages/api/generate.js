f8a014d478724aa2053641cab3cfea55ff547fe4
Raven The wolf <theoccultcorner@gmail.com>
2023-04-16 07:33:16 -0700

    FGFD

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,7 +10,10 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
+  console.log('Received HTTP request with body:', req.body);
+  
   if (!configuration.apiKey) {
+    console.log('OpenAI API key not configured');
     res.status(500).json({
       error: {
         message: "OpenAI API key not configured, please follow instructions in README.md",
@@ -21,6 +24,7 @@ export default async function handler(req, res) {
 
   const { name, phone, email, text } = req.body;
   if (!name || !phone || !email || !text) {
+    console.log('Missing required form fields');
     res.status(400).json({
       error: {
         message: "Please fill in all required fields",
@@ -36,7 +40,11 @@ export default async function handler(req, res) {
       temperature: 0.6,
       max_tokens: 100,
     });
+    console.log('OpenAI API response:', completion);
+    
     const messageBody = `Form Sumbitted\n${completion.data.choices[0].text}`;
+    console.log('Sending text message with body:', messageBody);
+    
     client.messages
       .create({
         body: messageBody,
@@ -45,6 +53,7 @@ export default async function handler(req, res) {
       })
       .then(message => console.log(`Message sent to ${message.to}: ${message.body}`))
       .catch(error => console.error(`Error sending message: ${error.message}`));
+      
     res.status(200).json({ result: "Message sent successfully!"  });
   } catch(error) {
     console.error(`Error with OpenAI API request: ${error.message}`);
