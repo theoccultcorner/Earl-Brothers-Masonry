867e6b7578c886f5ed42ddd8584b8ac8ecf5d51f
Raven The wolf <theoccultcorner@gmail.com>
2023-08-24 15:48:35 -0700

    sddg

--- pages-sections/Components-Sections/SectionDownload.js
+++ pages-sections/Components-Sections/SectionDownload.js
@@ -47,7 +47,7 @@ export default function SectionDownload() {
             
               target="_blank"
             
-              href="mailto:Earlbrothersenterprize@gmail.com">
+              href="mailto:Earlbrothersmasonry@gmail.com">
               Contact Us
             </Button>
           </GridItem>
